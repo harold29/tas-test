28e0d2c834fa6640260ff8eeab44ae8cdf403d91
Harold Perez <haroldjose2903@gmail.com>
2018-06-22 15:21:25 -0500

    Controller for users ready. User primary key changed. Routes changed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,38 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
 
-    def index
-        @users = User.all
-        render json: @users
-    end
+  def index
+    @users = User.all
+    # render json: @users
+    json_response(@users)
+  end
 
-    def show
-        if stale?(last_modified: @user.updated_at, public: true)
-            render json: @user
-        end
-    end
+  def show
+    json_response(@user)
+  end
 
-    def create
-        @user = User.create!(user_params)
-        json_response(@user, :created)
-    end
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
 
-    def update
-        @user.update(user_params)
-        head :no_content
-    end
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
 
-    def destroy
-        @user.destroy
-        head :no_content
-    end
+  def destroy
+    @user.destroy
+    head :no_content
+  end
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:email, :first_name, :last_name, :phone_number, :address)
-    end
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :phone_number, :address)
+  end
 
-    def set_user
-        @user = User.find(params(:email))
-    end
+  def set_user
+    @user = User.find(params.require(:id))
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_06_18_202617) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", id: false, force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_06_18_202617) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
